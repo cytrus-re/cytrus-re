f69291adc2c03c39108b20e3a7b18a6f7d9be1a0
Ed Luff <beartechtalks@gmail.com>
2019-08-16 20:43:56 +0100

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
    let log = 'RexoV1.0:\n' + 'Removed a bunch of unused/garbage commands.\n' + 'More grammar fixes (thanks Rexo and EDGE!)\n' + 'Coming soon: ???';
-   require('../changelog').forEach((change) => log += '\n- ' + change);
+   require('../modules/changelog').forEach((change) => log += '\n- ' + change);
   
    message.channel.send(log);
   } catch (err) {
