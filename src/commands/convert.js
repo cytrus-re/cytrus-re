df9533743314d8a150ec1a10027892655b927e31
Ed Luff <beartechtalks@gmail.com>
2019-11-14 22:01:35 +0000

    error and move

--- src/commands/convert.js
+++ src/commands/convert.js
@@ -29,7 +29,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
       message.reply('You have to choose C or F!');
     }
   } catch (err) {
-    message.channel.send('There was an error!\n' + err).catch();
+    message.channel.send(client.errors.genericError + err).catch();
   }
 };
 
