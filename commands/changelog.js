41098c6ba9cf77213dd13ef0ba139dca1a8e24d6
DestroyedEDGE <44955931+DestroyedEDGE@users.noreply.github.com>
2019-11-27 14:40:01 -0700

    fixed a crash

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-   let log = embed: { color: 0xFFFFFF, title: "**V1.1.0**", fields: [{ name: "Grammar", value: "More grammar fixes." }, { name: "Minor improvements", value: "Mostly code stuff." }] }
+   let log =  { color: 0xFFFFFF, title: "**V1.1.0**", fields: [{ name: "Grammar", value: "More grammar fixes." }, { name: "Minor improvements", value: "Mostly code stuff." }] }
    message.channel.send(log);
   } catch (err) {
    message.channel.send('There was an error!\n' + err).catch();
