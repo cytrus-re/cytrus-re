00878ababb69da2d92cc3ce292b24c4cd20cc24c
DestroyedEDGE <44955931+DestroyedEDGE@users.noreply.github.com>
2019-11-27 14:54:32 -0700

    Revert note.js to version from OldCytrus

--- commands/note.js
+++ commands/note.js
@@ -7,46 +7,48 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
         msg = await message.channel.send('Creating note...');
         
         await client.notes.set(message.author.id+message.id, {txt: args.slice(1).join(' '), id: message.author.id+message.id, author: message.author.id});
-        msg.edit('Note created with the ID of ' + message.author.id+message.id + '.');
+        msg.edit('Note created with the ID of ' + message.author.id+message.id);
         break;
       case 'remove':
-        if (!args[1]) return message.reply('You need to give the NoteID! Example: cy.note view 315843700490240002596675530208837642.');
-        if (client.notes.has(args[1])) 
-          if (client.notes.get(args[1]).author !== message.author.id) message.reply("You aren't the owner of the note you specified!");
+        if (!args[1]) return message.reply('You need to input the NoteID');
+        if (client.notes.has(args[1])) {
+          if (client.notes.get(args[1]).author !== message.author.id) message.reply('You dont own this note!');
           else {
             msg = await message.channel.send('Deleting note...');
-           
+
             await client.notes.delete(args[1]);
             msg.edit('Note deleted with the ID of ' + args[1]);
           }
-        } message.reply("That's not a valid NoteID!");
-     'clear';
+        } else message.reply('That is not a valid NoteID!');
+        break;
+      case 'clear':
         await client.notes.forEach((note)  => {
           if (note.author == message.author.id) client.notes.delete(note.id);
         });
+        
         message.channel.send('Cleared your notes!');
-    
-      'view';
-        if (!args[1]) return message.reply('You need to give the NoteID!');
+        break;
+      case 'view':
+        if (!args[1]) return message.reply('You need to input the NoteID');
         if (client.notes.has(args[1])) {
           if (client.notes.get(args[1]).author !== message.author.id) message.reply('You dont own this note!');
           else message.channel.send('•' + '*' + client.notes.get(args[1]).id + '*\n' + client.notes.get(args[1]).txt + '\n\n');
         } else message.reply('That is not a valid NoteID!');
-        
-      'edit';
-        if (!args[1]) return message.reply('You need to give the NoteID!');
-        if (!args[2]) return message.reply('You need to give the note text!');
+        break;
+      case 'edit':
+        if (!args[1]) return message.reply('You need to input the NoteID');
+        if (!args[2]) return message.reply('You need to input the Note Text');
         if (client.notes.has(args[1])) {
-          if (client.notes.get(args[1]).author !== message.author.id) message.reply("You don't own this note!");
+          if (client.notes.get(args[1]).author !== message.author.id) message.reply('You dont own this note!');
           else {
             msg = await message.channel.send('Editing note...');
 
             await client.notes.set(args[1], {txt: args.slice(2).join(' '), id: args[1], author: message.author.id});
             msg.edit('Note edited with the ID of ' + args[1]);
           }
-        } message.reply("That's not a valid NoteID!");
-       
-        default:
+        } else message.reply('That is not a valid NoteID!');
+        break;
+      default:
           let output = '';
 
           await client.notes.forEach((note)  => {
@@ -72,6 +74,6 @@ exports.conf = {
 exports.help = {
   name: 'note',
   category: 'General',
-  description: 'Makes a note for you.',
-  usage: 'note [clear/add [text]/remove [id]/view [id]/edit [id]]'
+  description: 'Generates a note',
+  usage: 'note [clear/add [text]/remove [id]/view [id]]'
 };
