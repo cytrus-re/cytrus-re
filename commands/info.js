3ee12938479ed1815837fd60b476d2bfbdf39391
Ed Luff <beartechtalks@gmail.com>
2019-12-05 21:09:25 +0000

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -4,14 +4,14 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   try {
     const embed = new Discord.RichEmbed()
     .setColor('#eeeeee')
-    .setTitle('Cytrus Rewrite Github')
-    .setFooter('Made by CelestialCrafter#6830 and EnderGirlGamer#5370, Rewritten by EDGE#9573, Rexowogamer#1183, and Midou#9637')
-    .setDescription(`Github: [Production Github](https://github.com/Rexogamer/cytrus)
-Website: [Website](https://cytrus.ga)
-API: [API](https://api.cytrus.ga/api)
-Support Server: [Server](https://discord.gg/4WgsA2q)
-Issues: [Github](https://github.com/Rexogamer/cytrus/issues)
-Version: RexoV1.0`);
+    .setTitle('Cytrus-RE Github')
+    .setFooter('Made by CelestialCrafter#6830 and EnderGirlGamer#5370. Rewritten by EDGE#9573, Rexowogamer#1183, and Midou#9637')
+    .setDescription(`Github: [Repo](https://github.com/Rexogamer/cytrus-re)
+Website: None
+API: None
+Support Server: [Server](https://discord.gg/BfpMgXs)
+Issues: [Github](https://github.com/Rexogamer/cytrus-re/issues)
+Version: V1.1`);
 
     message.channel.send(embed);
   } catch (err) {
