b7ba59be09430b26db9fac5d2553c724fbf9e2c6
Ed Luff <beartechtalks@gmail.com>
2019-12-09 15:25:25 +0000

    Boop

---
+++ src/commands/createchannel.js
@@ -0,0 +1,28 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  try {
+    if (!args[0]) return message.reply("You need to give me the channel name!");
+    if (!args[1]) return message.reply("You need to give me the channel type!");
+    
+    message.channel.send("I've created the channel!").then(() => {
+      message.guild.createChannel(args[1], args[0], []).catch((err) => {
+        message.channel.send(client.errors.genericError)
+      });
+    });
+  } catch (err) {
+    message.channel.send(client.errors.genericError + err).catch();
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  aliases: ["crc", "chanmake"],
+  guildOnly: true,
+  permLevel: "Administrator"
+};
+
+exports.help = {
+  name: "createchannel",
+  category: "Moderation",
+  description: "Creates a channel in the server.",
+  usage: "createchannel <voice/text> <name>"
+};
