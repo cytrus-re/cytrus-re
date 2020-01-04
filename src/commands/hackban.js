61916f91dd879d9b91eb7628e69d656e7f95900e
Ed Luff <beartechtalks@gmail.com>
2020-01-04 21:33:34 +0000

    Update and rename commands/forceban.js to src/commands/hackban.js

--- src/commands/hackban.js
+++ src/commands/hackban.js
@@ -7,36 +7,36 @@ exports.run = async (client, message, args, level) => {
 
     if (user) {
         message.guild.ban(user).then(() => {
-          message.reply('Successfully banned the user!');
+          message.reply("Successfully banned the user!");
 
           const modLogChannel = settings.modLogChannel;
           if (modLogChannel && message.guild.channels.find(c => c.name === settings.modLogChannel)) {
             let embed = new Discord.RichEmbed()
-            .setTitle('User Banned')
-            .setColor('#eeeeee')
-            .setDescription(`Name: ${user.username}\nID: ${args[0]}\nReason: ${args.slice(1).join(' ')}\nModerator: ${message.author.username}`);
+            .setTitle("User Banned")
+            .setColor("#eeeeee")
+            .setDescription(`Name: ${user.username}\nID: ${args[0]}\nReason: ${args.slice(1).join(" ")}\nModerator: ${message.author.username}`);
 
             message.guild.channels.find(c => c.name === settings.modLogChannel).send(embed);
           }
         }).catch(err => {
-          message.reply('I was unable to ban the user.');
+          message.reply("I was unable to ban the user.");
         });
-    } else message.channel.send("You didn't provide a valid UserID!");
+    } else message.channel.send("You didn't provide a valid user ID!");
   } catch (err) {
-    message.channel.send('There was an error!\n' + err +'').catch();
+    message.channel.send(client.errors.genericError).catch();
   }
 };
 
 exports.conf = {
   enabled: true,
-  aliases: ['fb'],
+  aliases: ["hb"],
   guildOnly: true,
-  permLevel: 'Moderator'
+  permLevel: "Moderator"
 };
 
 exports.help = {
-  name: 'forceban',
-  category: 'Moderation',
-  description: 'Bans a member not in your server.',
-  usage: 'forceban @<userID> [reason]'
+  name: "hackban",
+  category: "Moderation",
+  description: "Bans a member not in your server.",
+  usage: "hackban @<userID> [reason]"
 };
