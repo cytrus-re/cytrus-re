2ec55e41782779da303c01d770a4a78e6a9b6b5a
DestroyedEDGE <44955931+DestroyedEDGE@users.noreply.github.com>
2020-03-21 23:07:42 -0600

    updated changelog

--- src/commands/changelog.js
+++ src/commands/changelog.js
@@ -4,10 +4,11 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
    message.channel.send({embed:  
                  { author: { name: client.user.username, icon_url: client.user.avatarURL}, 
                   color: 0x1167b1,
-                  title: "**V1.1.1**",
+                  title: "**V1.1.2**",
                   fields: [{ name: "Grammar", value: "More grammar fixes."} ,
-                           { name: "Minor improvements", value: "Mostly code stuff." }],
-                  footer: {text: "By DestroyedEDGE#9573, Rexowogamer#1183, CelestialCrafter#7255, and Midou (notsoonline)#9637"
+                           { name: "Minor improvements", value: "Fixed some bugs that caused the bot to crash, and cleaned up repo." }
+                           { name: "Major improvements", value: "Added a new command (report)"}],
+                  footer: {text: "By DestroyedEDGE#8847, Rexowogamer#1183, CelestialCrafter#7255, and Midou#9637"
     } }});
   } catch (err) {
    message.channel.send(client.errors.genericError + err).catch();
