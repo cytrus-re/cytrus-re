e86e492243e6480e9935132aee9c36ee48b950ca
DestroyedEDGE <44955931+DestroyedEDGE@users.noreply.github.com>
2020-03-22 12:18:00 -0600

    Update warn.js

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -2,11 +2,15 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args, level) => {
   try {
-    const user = message.mentions.users.first();
+    const wUser = message.mentions.users.first();
     const settings = client.getSettings(message.guild.id);
+    const modLogChannel = settings.modLogChannel;
+    if(wUser.hasPermission(MANAGE_MESSAGES)) return message.reply("I can't warn that user!")
+    if(wUser.hasPermission(ADMINISTRATOR)) return message.reply("I can't warn that user!")
+    if(wUser.hasPermission(MANAGE_MEMBERS)) return message.reply("I can't warn that user!")
 
-    if (user) {
-      const member = message.guild.member(user);
+    if (wUser) {
+      const member = message.guild.member(wUser);
       if (member) {
         if (!client.warns.get(message.guild.id)) client.warns.set(message.guild.id, {});
         if (!client.warns.get(message.guild.id)[member.id]) client.warns.get(message.guild.id)[member.id] = 0;
@@ -14,24 +18,27 @@ exports.run = async (client, message, args, level) => {
         client.warns.get(message.guild.id)[member.id] += 1;
         message.reply(`Successfully warned ${user.tag}`);
 
-        const modLogChannel = settings.modLogChannel;
+        
+        
         if (modLogChannel && message.guild.channels.find(c => c.name === settings.modLogChannel)) {
           let embed = {
             color: 0xeeeeee,
             title: "User Warned",
-            description: `Name: ${user.username}\nID: ${user.id}\nModerator: ${message.author.username}`,
+            description: `Name: ${user.username}\nID: ${wUser.id}\nModerator: ${message.author.username}`,
             footer: { text: `${client.config.botName}` },
           };
           message.guild.channels.find(c => c.name === settings.modLogChannel).send(embed);
+          
+          wUser.send(`You have been warned in ${message.guild.name}.`)
         }
 
         if (client.warns.get(message.guild.id)[member.id] == 3) {
           member.ban(args.slice(1).join(' ')).then(() => {
-            message.channel.send(`Successfully banned ${user.tag}!`);
+            message.channel.send(`Successfully banned ${wUser.tag}!`);
 
             client.warns.get(message.guild.id)[member.id] = 0;
           }).catch(err => {
-            message.send(`I was unable to ban ${user.tag} for exceeding {client.config.maxWarns} warns!`);
+            message.send(`I was unable to ban ${wUser.tag} for exceeding ${client.config.maxWarns} warns!`);
           });
         }
       } else {
