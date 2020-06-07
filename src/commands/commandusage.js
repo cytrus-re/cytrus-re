f3b0f65d19b084fa942cbd586d21b6113b5248cb
Ed L <beartechtalks@gmail.com>
2020-06-07 09:14:21 +0100

    Update commandusage.js

--- src/commands/commandusage.js
+++ src/commands/commandusage.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
    try {
-     let output = '';
-     client.uses.forEach((cmd) => output += cmd + '\n');
+     let output = "";
+     client.uses.forEach((cmd) => output += cmd + "\n");
      message.channel.send(output)
    } catch (err) {
      message.channel.send(client.errors.genericError + err).catch();
