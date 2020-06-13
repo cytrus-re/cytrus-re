7c20c5a66d61a72ef6f5f09dbef8a6059307bc90
DestroyedEDGE <44955931+DestroyedEDGE@users.noreply.github.com>
2020-06-13 15:13:17 -0600

    rexo is very big brain

--- modules/errors.js
+++ modules/errors.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
-  genericError: "There was an error!\n",
+  genericError: "There was an error!\n"
   wiiError: "An error has occurred. \n Press the Eject button, remove the Game Disc, and turn off the power to the console.\n Please read the Wii Operations Manual for further instructions."
   userNotInGuild; "This user isn't in this guild!"
 };
