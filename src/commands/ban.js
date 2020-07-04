b25a04315125838a55a256930eec894c67326cfe
Ed L <beartechtalks@gmail.com>
2020-07-04 16:11:10 +0100

    ban: docs are 👌

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,18 +1,18 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); // Not sure if we really need this, since D.JS  is included in the client, but better safe than sorry.
 
 exports.run = async (client, message, args, level) => {
   try {
     const user = message.mentions.users.first();
-    const settings = client.getSettings(message.guild.id);
+    const settings = client.getSettings(message.guild.id); // Gets the guild's CyRE settings to check if the modlog feature is enabled (and if it is, what channel to send them to.)
 
     if (user) {
       const member = message.guild.member(user);
       if (member) {
         member.ban(args.slice(1).join(' ')).then(() => {
-          message.reply("Successfully banned ${user.tag}!");
+          message.channel.send("Successfully banned ${user.tag}!");
 
           const modLogChannel = settings.modLogChannel;
-          if (modLogChannel && message.guild.channels.find(c => c.name === settings.modLogChannel)) {
+          if (modLogChannel && message.guild.channels.find(c => c.name === settings.modLogChannel)) { // Checks if the modlog is enabled and the channel specified for it exists.
             let embed = new Discord.RichEmbed()
             .setTitle("User Banned")
             .setColor("#eeeeee")
@@ -20,16 +20,16 @@ exports.run = async (client, message, args, level) => {
 
             message.guild.channels.find(c => c.name === settings.modLogChannel).send(embed);
           }
-        }).catch(err => {
-         message.reply("I was unable to ban the user!");
+        }).catch(err => { // if banning failed for whatever reason, then:
+         message.channel.send("I was unable to ban the user!");
         });
-      } else {
-        message.reply("That user isn't in this guild!");
+      } else { // if the user isn't in the guild, then:
+        message.channel.send("That user isn't in this guild!");
       }
-    } else {
-      message.reply("You didn't mention the user you want to ban!");
+    } else { // if they didn't specify a user/did it incorrectly, then:
+      message.channel.send("You didn't mention the user you want to ban!");
     }
-  } catch (err) {
+  } catch (err) { // if something else broke, then:
     message.channel.send(client.errors.genericError + err).catch();
   }
 };
