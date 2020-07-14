3b3044eca5465cb3bc095c4661cc06e218ffb817
Ed L <beartechtalks@gmail.com>
2020-07-14 21:14:57 +0100

    currentos: improvements :)

--- src/commands/currentos.js
+++ src/commands/currentos.js
@@ -1,13 +1,28 @@
-// Oxygemo 2020
-// I don't have any rights reserved. But the MIT License is still in place.
+// Oxygemo/Rexogamer 2020
+// I don't have any rights reserved, but the MIT License is still in place. - Oxygemo (although Rexo says the same)
 const Discord = require("discord.js");
+const os = require("os"); // Node.JS core module, required for getting the OS
 const exec = require("child_process").exec; // This will allow us to do commands in the user shell/command prompt, which is neccesary for this to work.
 exports.run = async (client, message, args, level) => {   
     try {
-        await exec("uname");
-        message.reply("This instance of Cytrus-RE is running on a **UNIX-Like** OS or kernel (**Linux/OSX**).");    
+        var plat;
+        // var ver; // Added in Node 14, don't uncomment until upgrade
+        switch (new os.platform()) {
+            case "win32":
+                plat = "Windows";
+                break;
+            case "linux":
+                plat = "Linux"
+                break;
+            case "darwin"
+                plat = "Mac"
+                break;
+            default:
+                plat = "another OS"
+        }
+        message.channel.send(`This instance of Cytrus-RE is running on **${plat}**.`);
     } catch (err) {
-        message.channel.send("This instance of Cytrus-RE is running on **Windows**.");
+        message.channel.send(client.errors.genericError);
     }
     
 }
