5b1a564de5351f386c3d17487d93186d74264b13
Alexander Johansen <alej0hio2007@gmail.com>
2020-08-07 13:20:33 +0200

    Ready.js improvements
    Signed-off-by: Alexander Johansen <alej0hio2007@gmail.com>

--- events/ready.js
+++ events/ready.js
@@ -2,17 +2,22 @@ const request = require('request');
 
 module.exports = async client => {
   const statusList = [
-    {msg: `for commands | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
-    {msg: `with async/await errors | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
-    {msg: `with unhandled promise rejections | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
-    {msg: `with linux permissions | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
-    {msg: `Discord be slow | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
-    {msg: `over ${client.guilds.size} servers | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
-    {msg: `to EDGE rambling | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'LISTENING'},
-    {msg: `development progress! | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
-    {msg: `with Git and learning at the same time! | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
+    {msg: `Cytrus-RE 1.3.0 | ${client.config.defaultSettings.prefix}help  | Check out our ${client.config.defaultSettings.prefix}site!`, type: 'PLAYING'},
+    //{msg: `for commands | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
+    //{msg: `with async/await errors | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
+    //{msg: `with unhandled promise rejections | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
+    //{msg: `with linux permissions | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
+    //{msg: `Discord be slow | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
+    //{msg: `over ${client.guilds.size} servers | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
+    //{msg: `to EDGE rambling | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'LISTENING'},
+    //{msg: `development progress! | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
+    //{msg: `with Git and learning at the same time! | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
   ];
-
+    // Why did I comment out the rest? 
+    // If we're gonna try to be a serious bot, then we need to have serious status messages. 
+    // "playing with linux permissions" this comes from a kid who's too scared to use linux but ok
+    // "to EDGE rambling" if I was a normal random user I wouldn't know who EDGE was
+    // "with Git" sorry what
   setInterval(async () => {
     let index = Math.floor(Math.random() * statusList.length + 1) - 1;
     await client.user.setActivity(statusList[index].msg, {
@@ -20,11 +25,13 @@ module.exports = async client => {
     });
   }, 5000);
   
- setInterval(async () => {
-    request('https://web.cytrus.ga', (err, res, html) => {
-      if (err) client.logger.error(err);
-    });
-}, 28000);
+
+  // uncomment and update this once we get our own dashboard
+ // setInterval(async () => {
+ //   request('https://web.cytrus.ga', (err, res, html) => {
+ //     if (err) client.logger.error(err);
+ //   });
+//}, 28000);
 
 
   client.user.setStatus('online');
@@ -40,7 +47,7 @@ module.exports = async client => {
   //Starts the web server/API
   require('../modules/web')(client);
 
-  client.logger.log('Cytrus V' + require('../package').version + ' | https://github.com/Rexogamer/cytrus-re');
+  client.logger.log('Cytrus-RE V' + require('../package').version + ' | https://github.com/Cytrus-RE/cytrus-re');
   client.startuptime = new Date().getTime() - client.starttime;
   client.logger.log('It took ' + client.startuptime + 'ms to start Cytrus');
 };
