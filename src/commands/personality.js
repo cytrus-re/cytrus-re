ff689542f5bc61175c1626668bfeae16464dd1b2
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-08 12:21:08 +0200

    Update personality.js

--- src/commands/personality.js
+++ src/commands/personality.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 const types = ['Psychopath', 'Depressed', 'Cheerful', 'Bright', 'Dark', 'God', 'Deceiver', 'Funny', 'Fishy', 'Cool', 'Insecure', 'Lonely', 'Optimistic', 'Brave', 'Brilliant', 'Dreamer', 'Nurturer', 'Peaceful', 'Hot', 'Sexy', 'Perfect', 'Overthinker', 'Idealist'];
 const social = ['Loser', 'The nice guy', 'The cute girl', 'Dank memer', 'Nerd', 'Kinky'];
 const relationship = ['Single', 'Married', 'Taken', 'Forever alone'];
-const hobbies = ['Art', 'Drawing', 'Painting', 'Singing', 'Writing', 'Anime', 'Memes', 'Minecraft', 'Subscribing to PewDiePie from alt accounts', 'Deleting T-Series'];
+const hobbies = ['Art', 'Drawing', 'Painting', 'Singing', 'Writing', 'Anime', 'Memes', 'Minecraft', 'Coding'];
+// Are ya coding son?
 const genres = ['Nightcore', 'Heavy Metal', 'Alternative', 'Electronic', 'Classical', 'Dubstep', 'Jazz', 'Pop', 'Rap', 'Country'];
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
