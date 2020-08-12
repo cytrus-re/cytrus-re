80f32ef476fc6dd13f6c097662bc40ea218d053c
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-12 18:05:28 +0200

    if this brick the bot i as m esory

--- src/commands/copypasta.js
+++ src/commands/copypasta.js
@@ -1,9 +1,9 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
     try {
         // This code is GARBAGE. We could make it much better if we just read the copypastas from a file and caching it
-        if (args = "interjection")
+        if (!args["interjection"])
             message.channel.send("I'd just like to interject for a moment. \n What you're referring to as \'Linux', is in fact, GNU/Linux, or as I've recently taken to calling it, GNU + Linux. \n Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components compromising a full OS as defined by POSIX. \n Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called");
-        if (args = "testing")
+        if (!args["interjection"])
             message.channel.send("Yes, hello, we can hear you.");
     } catch (err) {
       message.channel.send(client.errors.genericError + err).catch();
