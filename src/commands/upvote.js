85744feae010a62b69ca2063feb221b8ba7259a8
Ed L <beartechtalks@gmail.com>
2020-08-12 19:00:38 +0100

    Update upvote.js

--- src/commands/upvote.js
+++ src/commands/upvote.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-    message.channel.send("Upvoting the bot on discordbotlist.com helps other people discover Cytrus-RE! Thanks for voting! \n https://discordbotlist.com/bots/cytrus-re");
+    message.channel.send("Upvoting the bot on discordbotlist.com helps other people discover Cytrus-RE! Thanks for voting!\n Vote here: https://discordbotlist.com/bots/cytrus-re");
   } catch (err) {
     message.channel.send(client.errors.genericError + err).catch();
   }
@@ -19,8 +19,8 @@ exports.conf = {
 //what the command does
 
 exports.help = {
-  name: 'upvote',
-  category: 'system',
-  description: 'Upvote the bot on discord bot list',
-  usage: 'cyre.upvote'
+  name: "upvote",
+  category: "system",
+  description: "Upvote the bot on Discord Bot List.",
+  usage: "upvote"
 };
