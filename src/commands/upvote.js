8948624eff600d9313048eba58ce055ee58be4db
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-12 16:32:19 +0200

    upvote.js: exist

---
+++ src/commands/upvote.js
@@ -0,0 +1,26 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  try {
+    message.channel.send("Upvoting the bot on discordbotlist.com helps other people discover Cytrus-RE! Thanks for voting! \n https://discordbotlist.com/bots/cytrus-re");
+  } catch (err) {
+    message.channel.send(client.errors.genericError + err).catch();
+  }
+};
+
+//permissions and aliases
+
+exports.conf = {
+  enabled: true,
+  aliases: [vote],
+  guildOnly: false,
+  permLevel: 'User'
+};
+
+
+//what the command does
+
+exports.help = {
+  name: 'upvote',
+  category: 'system',
+  description: 'Upvote the bot on discord bot list',
+  usage: 'cyre.upvote'
+};
