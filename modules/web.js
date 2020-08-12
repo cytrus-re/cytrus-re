7b7d5082ac40ac61c6f8b39eccb9d9b62e71f433
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-12 18:16:30 +0200

    lol

--- modules/web.js
+++ modules/web.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const logger = require('../modules/Logger');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 80;
 
 const initWeb = (client) => {
   app.set('view engine', 'ejs');
