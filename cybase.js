d8c243617d9e0683e1eecba127e50f273acd2f0a
Ed L <beartechtalks@gmail.com>
2020-08-12 17:20:37 +0100

    Update cybase.js

--- cybase.js
+++ cybase.js
@@ -1,24 +1,24 @@
 'use strict';
 
-//Check if the node version is 10.0.0+
-if (Number(process.version.slice(1).split('.')[0]) < 10) throw new Error('NodeJS 10.0.0 or higher is required. Re-run this with NodeJS 10.0.0+');
+// Check if the Node version is 10+
+if (Number(process.version.slice(1).split('.')[0]) < 10) throw new Error("Node 10 or higher is required. Re-run this with Node 10 or higher. (NOTE: Node 14 will be required soon.)");
 if (process.env.PREBOOT) eval(process.env.PREBOOT);
 
-//Define NPM modules
+// Define NPM modules
 const Discord = require('discord.js');
 const Enmap = require('enmap');
 const CleverBotAPI = require('cleverbot.io');
 
-//Define client
+// Define client
 const client = new Discord.Client({
   disableEveryone: true,
   disabledEvents: ['TYPING_START']
 });
 
-//Define time of startup
+// Define time of startup
 client.starttime = new Date();
 
-//Define Databases/Objects
+// Define databases/objects
 client.items = new Enmap({name: 'glptmitems'});
 client.money = new Enmap({name: 'glptm'});
 client.profiles = new Enmap({name: 'profiles'});
@@ -38,11 +38,11 @@ client.liusers = new Discord.Collection();
 client.music = {};
 client.levelCache = {};
 
-//Define CleverBot API
+// Define CleverBot API
 client.cleverbot = new CleverBotAPI(process.env.CLEVERBOT_USER_KEY, process.env.CLEVERBOT_API_KEY);
 client.cleverbot.setNick('CytrusBot');
 
-//Generate session secret
+// Generate session secret for the site
 process.env.SESSION_SECRET = '';
 for (let i = 0; i <= 1500; i++)
   process.env.SESSION_SECRET += Math.random()
@@ -51,7 +51,7 @@ for (let i = 0; i <= 1500; i++)
   .toUpperCase()
   .slice(-6) + i;
 
-//Import files
+// Import files
 client.logger = require('./modules/Logger');
 client.config = require('./cnf');
 client.errors = require('./modules/errors');
@@ -59,15 +59,15 @@ require('./modules/commands')(client);
 require('./modules/events')(client);
 require('./modules/_functions')(client);
 
-//Cache the permissions
+// Cache the permissions
 for (let i = 0; i < client.config.permLevels.length; i++) {
   let currentlevel = client.config.permLevels[i];
   client.levelCache[currentlevel.name] = currentlevel.level;
 }
 
-//Login to discord
+// Login to Discord
 client.login(process.env.BOT_TOKEN);
 
-//Export the client
+// Export the client
 module.exports = client;
 
