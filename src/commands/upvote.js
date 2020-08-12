612aa5dafa2857b1169b2967b82b4a208c600fa1
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-12 16:39:59 +0200

    upvote.js: fix bug
    Same as https://github.com/Cytrus-RE/cytrus-re/commit/bcf973bbd8aed4e0b08a8d158880d54b86e2372d

--- src/commands/upvote.js
+++ src/commands/upvote.js
@@ -10,7 +10,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 
 exports.conf = {
   enabled: true,
-  aliases: [vote],
+  aliases: ["vote"],
   guildOnly: false,
   permLevel: 'User'
 };
