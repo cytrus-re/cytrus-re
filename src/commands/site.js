239dfe1b4a64e03b45568f7a0a411196a2439cdb
Ed L <beartechtalks@gmail.com>
2020-08-12 17:09:41 +0100

    Update site.js

--- src/commands/site.js
+++ src/commands/site.js
@@ -19,8 +19,8 @@ exports.conf = {
 //what the command does
 
 exports.help = {
-  name: 'Site',
+  name: 'site',
   category: 'system',
   description: 'Send a message to this channel with a link to our (totally good) website hosted on GitHub Pages!',
-  usage: 'cyre.site'
+  usage: 'site'
 };
