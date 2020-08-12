9a24f7bae33be342fab7dfce8cd41eec1eb49ccd
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-12 17:43:21 +0200

    Create site.js

---
+++ src/commands/site.js
@@ -0,0 +1,26 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  try {
+    message.channel.send("https://cytrus-re.github.io");
+  } catch (err) {
+    message.channel.send(client.errors.genericError + err).catch();
+  }
+};
+
+//permissions and aliases
+
+exports.conf = {
+  enabled: true,
+  aliases: ["cytrusresite", "webpage", "page"],
+  guildOnly: false,
+  permLevel: 'User'
+};
+
+
+//what the command does
+
+exports.help = {
+  name: 'Site',
+  category: 'system',
+  description: 'Send a message to this channel with a link to our (totally good) website hosted on GitHub Pages!',
+  usage: 'cyre.site'
+};
