2c4bb9ed5f3418ae79bab121fdaef34c1dec2e74
Ed L <beartechtalks@gmail.com>
2020-08-12 17:27:12 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,14 +3,14 @@ const request = require('request');
 module.exports = async client => {
   const statusList = [
     {msg: `Cytrus-RE 1.3.0 | ${client.config.defaultSettings.prefix}help  | Check out our ${client.config.defaultSettings.prefix}site!`, type: 'PLAYING'},
-    //{msg: `for commands | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
+    {msg: `for commands | ${client.config.defaultSettings.prefix}help | Check out our ${client.config.defaultSettings.prefix}site!`, type: 'WATCHING'},
     //{msg: `with async/await errors | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
     //{msg: `with unhandled promise rejections | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
     //{msg: `with linux permissions | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
     //{msg: `Discord be slow | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
     //{msg: `over ${client.guilds.size} servers | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
     //{msg: `to EDGE rambling | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'LISTENING'},
-    //{msg: `development progress! | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
+    {msg: `development progress! | ${client.config.defaultSettings.prefix}help | Check out our ${client.config.defaultSettings.prefix}site!`, type: 'WATCHING'},
     //{msg: `with Git and learning at the same time! | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
   ];
     // Why did I comment out the rest? 
@@ -26,29 +26,29 @@ module.exports = async client => {
   }, 5000);
   
 
-  
-  setInterval(async () => {
-  request('https://cytrus-re.herokuapp.com/', (err, res, html) => {
-  if (err) client.logger.error(err);
-    });
-  }, 28000);
+  // uncomment and update this once we get our own dashboard
+ // setInterval(async () => {
+ //   request('https://web.cytrus.ga', (err, res, html) => {
+ //     if (err) client.logger.error(err);
+ //   });
+//}, 28000);
 
 
   client.user.setStatus('online');
 
   //Logs the Status
-  client.logger.log(`Ram Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, 'ready');
+  client.logger.log(`RAM usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, 'ready');
   client.logger.log(`Users: ${client.users.size.toLocaleString().replace(/ /g, '')}`, 'ready');
   client.logger.log(`Servers: ${client.guilds.size.toLocaleString().replace(/ /g, '')}`, 'ready');
   client.logger.log(`Channels: ${client.channels.size.toLocaleString().replace(/ /g, '')}`, 'ready');
-  client.logger.log(`Discord.js: v${require('discord.js').version.replace(/ /g, '')}`, 'ready');
-  client.logger.log(`Node.js: ${process.version.replace(/ /g, '')}`, 'ready');
+  client.logger.log(`Running on Node ${process.version.replace(/ /g, '')}`, 'ready');
+  client.logger.log(`and using Discord.js ${require('discord.js').version.replace(/ /g, '')}`, 'ready');
 
-  //Starts the web server/API
+  // Starts the web server/API
   require('../modules/web')(client);
 
-  client.logger.log('Cytrus-RE V' + require('../package').version + ' | https://github.com/Cytrus-RE/cytrus-re');
+  client.logger.log("Cytrus-RE V" + require("../package").version + " | https://github.com/Cytrus-RE/cytrus-re");
   client.startuptime = new Date().getTime() - client.starttime;
-  client.logger.log('It took ' + client.startuptime + 'ms to start Cytrus');
+  client.logger.log("It took " + client.startuptime + 'ms to start Cytrus-RE.");
 };
 
