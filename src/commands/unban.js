73266488f3e49b4a2ec678368a5b11e5fb8ab323
Ed L <beartechtalks@gmail.com>
2020-08-13 16:04:43 +0100

    Update unban.js

--- src/commands/unban.js
+++ src/commands/unban.js
@@ -6,39 +6,39 @@ exports.run = async (client, message, args, level) => {
     const settings = client.getSettings(message.guild.id);
 
     if (user) {
-        message.guild.unban(args.slice(1).join(' ')).then(() => {
-          message.reply(`Successfully unbanned ${user.tag}`);
+        message.guild.members.unban(args.slice(1).join(' ')).then(() => {
+          message.channel.send(`Successfully unbanned ${user.tag}!`);
 
           const modLogChannel = settings.modLogChannel;
           if (modLogChannel && message.guild.channels.find(c => c.name === settings.modLogChannel)) {
-            let embed = new Discord.RichEmbed()
-            .setTitle('User Unban')
-            .setColor('#eeeeee')
+            let embed = new Discord.MessageEmbed()
+            .setTitle("User Unbanned")
+            .setColor("#eeeeee")
             .setDescription(`Reason: ${args.slice(1).join(' ')}\nModerator: ${message.author.username}`);
 
             message.guild.channels.find(c => c.name === settings.modLogChannel).send(embed);
           }
         }).catch(err => {
-         message.reply('I was unable to unban the member');
+         message.reply("I was unable to unban the member!");
         });
     } else {
-      message.reply('You didn\'t give the UserID to unban!');
+      message.reply("You didn't specify the user to unban!");
     }
   } catch (err) {
-    message.channel.send('Their was an error!\n' + err +'').catch();
+    message.channel.send(client.errors.genericError + err).catch();
   }
 };
 
 exports.conf = {
   enabled: true,
-  aliases: ['b'],
+  aliases: ["b"],
   guildOnly: true,
-  permLevel: 'Moderator'
+  permLevel: "Moderator"
 };
 
 exports.help = {
-  name: 'unban',
-  category: 'Moderation',
-  description: 'Unbans a member for an optional reason',
-  usage: 'unban <userid> [reason]'
+  name: "unban",
+  category: "Moderation",
+  description: "Unbans a member.",
+  usage: "unban <userid> [reason]"
 };
