c8eaf88bc1c9d85720b8a6bc2820db88e8d7eb4d
Ed L <beartechtalks@gmail.com>
2020-08-13 10:37:39 +0100

    Update botinv.js

--- src/commands/botinv.js
+++ src/commands/botinv.js
@@ -1,21 +1,21 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-    client.generateInvite(['ADMINISTRATOR']).then(link => message.channel.send("Bot Invite: " + link));
+    message.channel.send("[**Click here!**](https://discordapp.com/api/oauth2/authorize?client_id=596304769333592078&permissions=2113404023&scope=bot)"));
   } catch (err) {
-    message.channel.send('There was an error!\n' + err).catch();
+    message.channel.send(client.errors.genericError + err).catch();
   }
 };
 
 exports.conf = {
   enabled: true,
-  aliases: ['ri', 'botinvite', 'bi', 'returninvite','cyinv'],
+  aliases: ["ri," "botinvite", "bi", "returninvite", "cyinv"],
   guildOnly: true,
-  permLevel: 'User'
+  permLevel: "User"
 };
 
 exports.help = {
-  name: 'botinv',
-  category: 'General',
+  name: "botinv",
+  category: "General",
   description: "Gives the invite link for Cytrus.",
-  usage: 'botinv'
+  usage: "botinv"
 };
