cd57c9033de4d7a79e023d6b7e626d7092f6054c
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-13 17:05:52 +0200

    Update errors.js

--- modules/errors.js
+++ modules/errors.js
@@ -2,7 +2,7 @@ const errors = {
   "genericError": "There was an error!\n",
   "wiiError": "An error has occurred. \n Press the Eject button, remove the Game Disc, and turn off the power to the console.\n Please read the Wii Operations Manual for further instructions.",
   "userNotInGuild": "This user isn't in this guild!",
-  "cannotBanSelf":  "Sorry, you can't ban yourself."
+  "cannotBanSelf":  "Sorry, you can't ban yourself. (It would be funny, if it wasn't so sad.)"
 };
 
 module.exports = errors;
