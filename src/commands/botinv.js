87db55c232382f3cad991f8655e8435cdb6ced45
Ed L <beartechtalks@gmail.com>
2020-08-13 10:49:25 +0100

    Update botinv.js

--- src/commands/botinv.js
+++ src/commands/botinv.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-    message.channel.send("[**Click here!**](https://discordapp.com/api/oauth2/authorize?client_id=596304769333592078&permissions=2113404023&scope=bot)"));
+    message.channel.send("[**Click here!**](https://discordapp.com/api/oauth2/authorize?client_id=596304769333592078&permissions=2113404023&scope=bot)");
   } catch (err) {
     message.channel.send(client.errors.genericError + err).catch();
   }
