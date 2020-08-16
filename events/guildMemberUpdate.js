c77f85f851f550199e27ac583ba234a8f0669f23
Ed L <beartechtalks@gmail.com>
2020-08-16 11:14:31 +0100

    I think that's it

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -12,8 +12,8 @@ module.exports = (client, user, userNew) => {
   
   if (user.nickname !== userNew.nickname) {
     embed = new Discord.MessageEmbed()
-    .setTitle('User Nickname Update')
-    .setColor('#eeeeee')
+    .setTitle("User Nickname Update")
+    .setColor("#eeeeee")
     .setFooter(`Full name: ${userNew.user.tag} | ID: ${userNew.id}`)
     .setDescription(`Old User:
 •Nickname: ${user.nickname}
@@ -25,8 +25,8 @@ New User:
   
   if (user.user.tag !== userNew.user.tag) {
      embed = new Discord.MessageEmbed()
-    .setTitle('Username Update')
-    .setColor('#eeeeee')
+    .setTitle("Username Update")
+    .setColor("#eeeeee")
     .setFooter(`Full name: ${userNew.user.tag} | ID: ${userNew.id}`)
     .setDescription(`Old User:
 •Full name ${user.user.tag}
@@ -36,44 +36,44 @@ New User:
     modLogChannel.send(embed).catch();
   }
 
-  if (user.cache.roles !== userNew.cache.roles) {
+  if (user.roles.cache !== userNew.roles.cache) {
     let output = '';
     let outputNew = '';
     
-    user.cache.roles.forEach(role => {
+    user.roles.cache.forEach(role => {
       output += '\n' + role.name;
     });
     
-    userNew.cache.roles.forEach(role => {
+    userNew.roles.cache.forEach(role => {
       outputNew += '\n' + role.name;
     });
     
     if (output == outputNew) return;
     
     embed = new Discord.MessageEmbed()
-    .setTitle('User Roles Update')
+    .setTitle("User Roles Update")
     .setFooter(`Full name: ${userNew.user.tag} | ID: ${userNew.id}`)
     .setDescription(`
 Old Roles${output}
 
 New Roles${outputNew}`)
-    .setColor('#eeeeee');
+    .setColor("#eeeeee");
     
     modLogChannel.send(embed).catch();
   }
   
   if (user.user.avatarURL !== userNew.user.avatarURL) {
     embed = new Discord.MessageEmbed()
-    .setTitle('User Avatar Update')
+    .setTitle("User Avatar Update")
     .setFooter(`Full name: ${userNew.user.tag} | ID: ${userNew.id}`)
     .setDescription(`
 Old Avatar URL: ${user.user.avatarURL}
 
 New Avatar URL: ${userNew.user.avatarURL}`)
-    .setColor('#eeeeee');
+    .setColor("#eeeeee");
     
     modLogChannel.send(embed).catch();
-    modLogChannel.send(new Discord.Attachment(user.user.avatarURL, 'old.png')).catch();
-    modLogChannel.send(new Discord.Attachment(userNew.user.avatarURL, 'new.png')).catch();
+    modLogChannel.send(new Discord.MessageAttachment(user.user.avatarURL, "old.png")).catch();
+    modLogChannel.send(new Discord.MessageAttachment(userNew.user.avatarURL, "new.png")).catch();
   }
 };
