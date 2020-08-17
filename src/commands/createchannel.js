81952832b2e40945a15c454792883287f57fb91d
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-17 15:12:36 +0200

    Update createchannel.js

--- src/commands/createchannel.js
+++ src/commands/createchannel.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     if (!args[0]) return message.reply("You need to give me the channel name!");
     if (!args[1]) return message.reply("You need to give me the channel type!");
     
-    message.channel.send("I've created the channel!").then(() => {
+    message.channel.send("Channel created.").then(() => {
       message.guild.createChannel(args[1], args[0], []).catch((err) => {
         message.channel.send(client.errors.genericError)
       });
