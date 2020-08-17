d51db0ce9010e2a1ac01becbe438bfec427ef1e1
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-17 20:03:14 +0200

    jaiknsdiojasjsaiodjosa yes

--- modules/errors.js
+++ modules/errors.js
@@ -3,7 +3,8 @@ const errors = {
   "wiiError": "An error has occurred. \n Press the Eject button, remove the Game Disc, and turn off the power to the console.\n Please read the Wii Operations Manual for further instructions.",
   "userNotInGuild": "This user isn't in this guild!",
   "cannotBanSelf":  "Sorry, you can't ban yourself. (It would be funny, if it wasn't so sad.)",
-  "noResults": "I couldn't find any results for your query!"
+  "noResults": "I couldn't find any results for your query!",
+  "404ArticleDescription": "This article doesn't have a description or something went wrong."
 };
 
 module.exports = errors;
