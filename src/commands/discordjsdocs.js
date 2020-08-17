e353c5aade898f66fa9d6c112d51e6dfbf451fa0
Ed L <beartechtalks@gmail.com>
2020-08-17 15:44:25 +0100

    Update discordjsdocs.js

--- src/commands/discordjsdocs.js
+++ src/commands/discordjsdocs.js
@@ -2,26 +2,26 @@ const request = require('request');
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-    if (!args[0]) return message.reply("You need to give something to search!");
+    if (!args[0]) return message.channel.send(client.errors.noQueryGiven);
     
     request({url: 'https://djsdocs.sorta.moe/main/stable/embed?q=' + encodeURIComponent(args.join(' ')), json: true}, (req, res, json) => {
       message.channel.send({embed: json});
     });
   } catch (err) {
-    message.channel.send("There was an error!\n" + err).catch();
+    message.channel.send(client.errors.genericError + err).catch();
   }
 };
 
 exports.conf = {
   enabled: true,
-  aliases: ['djs', 'djsdocs'],
+  aliases: ["djs", "djsdocs"],
   guildOnly: false,
-  permLevel: 'User'
+  permLevel: "User"
 };
 
 exports.help = {
-  name: 'discordjsdocs',
-  category: 'General',
-  description: "Searches the Discord.js docs for your search term.",
-  usage: 'discordjsdocs <query>'
+  name: "discordjsdocs",
+  category: "Search",
+  description: "Searches the Discord.js docs for your query.",
+  usage: "discordjsdocs <query>"
 };
