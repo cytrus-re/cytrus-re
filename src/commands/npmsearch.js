7954983983d5ff477eb9ab1ea9e73b5b59294f46
Ed L <beartechtalks@gmail.com>
2020-08-17 14:55:14 +0100

    Update npmsearch.js

--- src/commands/npmsearch.js
+++ src/commands/npmsearch.js
@@ -4,9 +4,9 @@ exports.run = async (client, message, args, level) => {
   try {
     let output = '';
     let i = 1;
-    if (!args[0]) return message.reply('You need to give me something to search!');
+    if (!args[0]) return message.channel.send(client.errors.noQueryGiven);
     
-    request({url: 'https://api.npms.io/v2/search?q=' + encodeURIComponent(args.join(' ')), json: true}, async (req, res, json) => {
+    request({url: "https://api.npms.io/v2/search?q=" + encodeURIComponent(args.join(' ')), json: true}, async (req, res, json) => {
       if (json.results.length > 5) json.results.length = 5;
       
       json.results.forEach((module) => {
@@ -14,8 +14,8 @@ exports.run = async (client, message, args, level) => {
         i++;
       });
       
-      let module = await client.awaitReply(message, "Please choose the module you want: ${output}");
-      if (isNaN(module)) return message.reply("That's not a number!");
+      let module = await client.awaitReply(message, "Please choose the package you want: ${output}");
+      if (isNaN(module)) return message.channel.send("That's not a number!");
       
       let mnum = Number(module) - 1;
       let package = json.results[mnum].package;
@@ -25,16 +25,16 @@ exports.run = async (client, message, args, level) => {
         description: package.description,
         url: package.links.npm,
         author: {
-          name: package.publisher.username || 'None Specified'
+          name: package.publisher.username || "No Author Specified"
         },
         fields: [
           {
-            title: 'Version',
+            title: "Version",
             text: package.version
           },
           {
-            title: 'Keywords',
-            text: package.keywords ? package.keywords.join(', ') : 'None'
+            title: "Keywords",
+            text: package.keywords ? package.keywords.join(', ') : "None"
           }
         ]
       });
@@ -42,20 +42,20 @@ exports.run = async (client, message, args, level) => {
       message.channel.send(embed);
     });
   } catch (err) {
-    message.channel.send('There was an error!\n' + err).catch();
+    message.channel.send(client.errors.genericError + err).catch();
   }
 };
 
 exports.conf = {
   enabled: true,
-  aliases: ['npm'],
+  aliases: ["npm"],
   guildOnly: false,
-  permLevel: 'User'
+  permLevel: "User"
 };
 
 exports.help = {
-  name: 'npmsearch',
-  category: 'General',
-  description: 'Searches the Node Package Manager for you.',
-  usage: 'npmsearch <query>'
+  name: "npmsearch",
+  category: "Search",
+  description: "Searches NPM for your query.",
+  usage: "npmsearch <query>"
 };
