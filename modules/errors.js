d21a1b7310eb20d5e8240293d0be1ecd7f592a52
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-21 18:59:10 +0200

    genericError: yes

--- modules/errors.js
+++ modules/errors.js
@@ -1,5 +1,5 @@
 const errors = {
-  "genericError": "There was an error!\n",
+  "genericError": "There was an error!\n Please send the following to a Cytrus-RE dev (Odyssey346#9848 or Rexowogamer#1183)\n",
   "wiiError": "An error has occurred. \n Press the Eject button, remove the Game Disc, and turn off the power to the console.\n Please read the Wii Operations Manual for further instructions.",
   "userNotInGuild": "This user isn't in this guild!",
   "cannotBanSelf":  "Sorry, you can't ban yourself. (It would be funny, if it wasn't so sad.)",
