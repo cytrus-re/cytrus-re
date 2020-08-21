ebda28709cf62620c56a8ef0ae131da894081100
Ed L <beartechtalks@gmail.com>
2020-08-21 12:52:44 +0100

    quick fix to bring it up

--- src/commands/unmute.js
+++ src/commands/unmute.js
@@ -30,7 +30,7 @@ exports.run = async (client, message, args, level) => {
       message.reply("You didn't mention the user to unmute!");
     }
   } catch (err) {
-    message.channel.send("Their was an error!\n" + err).catch();
+    message.channel.send(client.errors.genericError + err).catch();
   }
 };
 
