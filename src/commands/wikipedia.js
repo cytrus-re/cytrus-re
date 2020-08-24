2af6bad871b6cc71938ab50f2f7dca696fd040f9
Ed L <beartechtalks@gmail.com>
2020-08-24 18:05:09 +0100

    Update wikipedia.js

--- src/commands/wikipedia.js
+++ src/commands/wikipedia.js
@@ -20,22 +20,11 @@ exports.run = async (client, message, args) => {
       
       if (isNaN(page)) return message.channel.send(`${page} is not a number!`);
       let info = res.query.pages[Object.keys(res.query.pages)[page - 1]]
-<<<<<<< HEAD
       let infDesc = wikipedia.search(info.title, "en", { prop: "description" });
-=======
-      let infDesc = wikipedia.search(info.title, "en", { prop: "description });
->>>>>>> parent of 32b6af4f... rexo you idiot you didn't put a quotation mark
       let pageEmbed = new Discord.MessageEmbed()
       .setTitle(`${info.title} on Wikipedia`)
-<<<<<<< HEAD
-<<<<<<< HEAD
       .setDescription(`${infDesc.description ? infDesc.description : client.errors.noArticleDescription}`)
-=======
-      .setDescription(`${infDesc.description ? infDesc.description : client.error.404ArticleDescription }`)
->>>>>>> parent of 331df204... Small brain
-=======
       .setDescription(`${infDesc.description ? infDesc.description : "This article doesn't have a description or something went wrong" }`)
->>>>>>> parent of 58bc8e4b... Update wikipedia.js
       .addField("Article link", `[Right here!](${info.fullurl.replace("(", "\\(").replace(")", "\\)").replace("`", "\\`")})`)
       .setColor("#eeeeee");
 
