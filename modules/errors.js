fb601420e6d2ed62870ee73d36421e269bc12dbc
Odyssey346 <alej0hio2007@gmail.com>
2020-08-24 18:02:34 +0200

    owowowowwowowwowwwwww

--- modules/errors.js
+++ modules/errors.js
@@ -1,6 +1,11 @@
 const errors = {
+<<<<<<< HEAD
   "genericError": "There was an error!\nPlease send the following to a Cytrus-RE dev (Odyssey346#9848 or Rexowogamer#1183) and tell them command you used (including arguments).\n",
   "wiiError": "An error has occurred. \nPress the Eject button, remove the Game Disc, and turn off the power to the console. \nPlease read the Wii Operations Manual for further instructions.",
+=======
+  "genericError": "There was an error!\n Please send the following to a Cytrus-RE dev (Odyssey346#9848 or Rexowogamer#1183)\n",
+  "wiiError": "An error has occurred. \n Press the Eject button, remove the Game Disc, and turn off the power to the console.\n Please read the Wii Operations Manual for further instructions.",
+>>>>>>> parent of 7318a9d0... Update errors.js
   "userNotInGuild": "This user isn't in this guild!",
   "cannotBanSelf":  "Sorry, you can't ban yourself. (It would be funny, if it wasn't so sad.)",
   "noResults": "I couldn't find any results for your query!",
