e73eb3a590e024e34bbf7627f68e73d2d8579450
Odyssey346 <alej0hio2007@gmail.com>
2020-08-24 18:01:53 +0200

    owowowowwowowwowwwwww

--- src/commands/copypasta.js
+++ src/commands/copypasta.js
@@ -3,12 +3,16 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
         // This code is GARBAGE. We could make it much better if we just read the copypastas from a file and caching it
         if (args[0] == "interjection")
 <<<<<<< HEAD
+<<<<<<< HEAD
             message.channel.send(`I'd just like to interject for a moment. \n What you're referring to as 'Linux', is in fact, GNU/Linux, or as I've recently taken to calling it, GNU + Linux. \n Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components compromising a full OS as defined by POSIX.\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often calle-\n Triggered by <@${message.author}>`);
 =======
             message.channel.send(`I'd just like to interject for a moment. \n What you're referring to as 'Linux', is in fact, GNU/Linux, or as I've recently taken to calling it, GNU + Linux. \n Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components compromising a full OS as defined by POSIX.\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often calle-\n Triggered by <@${message.author)>}`;
 >>>>>>> parent of eea077c4... rexo fuck dumvb
+=======
+            message.channel.send(`I'd just like to interject for a moment. \n What you're referring to as 'Linux', is in fact, GNU/Linux, or as I've recently taken to calling it, GNU + Linux. \n Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components compromising a full OS as defined by POSIX.\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often calle-\nTriggered by ${message.author)}`;
+>>>>>>> parent of 799b0ce2... maybe this will fix the issue
         if (args[0] == "testing")
-            message.channel.send(`Yes, hello, we can hear you.\nTriggered by <@${message.author}>`);
+            message.channel.send(`Yes, hello, we can hear you.\nTriggered by ${message.author}`);
     } catch (err) {
       message.channel.send(client.errors.genericError + err).catch();
     }
