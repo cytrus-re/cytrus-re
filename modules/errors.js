20e109ec40eeed21b1c67028636cb05b8fac0eb0
Ed L <beartechtalks@gmail.com>
2020-08-24 18:06:59 +0100

    Update errors.js

--- modules/errors.js
+++ modules/errors.js
@@ -1,23 +1,10 @@
 const errors = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   "genericError": "There was an error!\nPlease send the following to a Cytrus-RE dev (Odyssey346#9848 or Rexowogamer#1183) and tell them command you used (including arguments).\n",
-  "wiiError": "An error has occurred. \nPress the Eject button, remove the Game Disc, and turn off the power to the console. \nPlease read the Wii Operations Manual for further instructions.",
-=======
-  "genericError": "There was an error!\n Please send the following to a Cytrus-RE dev (Odyssey346#9848 or Rexowogamer#1183)\n",
-=======
-  "genericError": "There was an error!\n",
->>>>>>> parent of d21a1b73... genericError: yes
-  "wiiError": "An error has occurred. \n Press the Eject button, remove the Game Disc, and turn off the power to the console.\n Please read the Wii Operations Manual for further instructions.",
->>>>>>> parent of 7318a9d0... Update errors.js
+  "wiiError": "An error has occurred.\nPress the Eject button, remove the Game Disc, and turn off the power to the console.\nPlease read the Wii Operations Manual for further instructions.",
   "userNotInGuild": "This user isn't in this guild!",
   "cannotBanSelf":  "Sorry, you can't ban yourself. (It would be funny, if it wasn't so sad.)",
-<<<<<<< HEAD
   "noResults": "I couldn't find any results for your query!",
   "noArticleDescription": "This article doesn't have a description or something went wrong."
-=======
-  "noResults": "I couldn't find any results for your query!"
->>>>>>> parent of d51db0ce... jaiknsdiojasjsaiodjosa yes
 };
 
 module.exports = errors;
