6cf676062abd0f299332d117decf658858781346
Odyssey346 <alej0hio2007@gmail.com>
2020-08-24 18:02:53 +0200

    Revert "Update youtube.js"
    This reverts commit 83208405c9000b63923f77382c8f52b8ab5dde87.

--- src/commands/youtube.js
+++ src/commands/youtube.js
@@ -6,7 +6,7 @@ const searcher = new YTSearcher(process.env.YOUTUBE_API_KEY);
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
     if (!args[0]) return message.reply("You need to give something to search!");
-    // waking up heroku
+    
     let msg = await message.channel.send("Searching YouTube...");
     
     searcher.search(args.join(" ")).then(info => {
