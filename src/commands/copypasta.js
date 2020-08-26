4a489518c8dccde1391fe5a348987d5a72051601
CommandBlock6417 <58105634+CommandBlock6417@users.noreply.github.com>
2020-08-26 18:40:51 +0300

    dam bracket

--- src/commands/copypasta.js
+++ src/commands/copypasta.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     try {
         // This code is GARBAGE. We could make it much better if we just read the copypastas from a file and caching it
         if (copypastas.pastaslist.includes(args[0])) {
-            message.channel.send(`${copypastas.${args[0]}\nTriggered by ${message.author}`);
+            message.channel.send(`${copypastas.${args[0]}}\nTriggered by ${message.author}`);
         } else {
             message.channel.send({ embed: { color: "#ff3333", title: "What's that?", description: "I don't know that copypasta. Maybe try `interjection` or `testing` instead?" } });
         }
