ac81d042cb8f5839ced0d3541d7c14669489205f
Ed L <beartechtalks@gmail.com>
2020-08-26 11:34:31 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async client => {
   const statusList = [
-    {msg: `Cytrus-RE 1.3.0 | ${client.config.defaultSettings.prefix}help  | Check out our ${client.config.defaultSettings.prefix}site!`, type: "PLAYING"},
+    {msg: `Cytrus-RE 1.3.0 | ${client.config.defaultSettings.prefix}help | Check out our ${client.config.defaultSettings.prefix}site!`, type: "PLAYING"},
     {msg: `for commands | ${client.config.defaultSettings.prefix}help | Check out our ${client.config.defaultSettings.prefix}site!`, type: "WATCHING"},
     //{msg: `with async/await errors | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
     //{msg: `with unhandled promise rejections | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
@@ -15,7 +15,7 @@ module.exports = async client => {
     // If we're gonna try to be a serious bot, then we need to have serious status messages. 
     // "playing with linux permissions" this comes from a kid who's too scared to use linux but ok
     // "to EDGE rambling" if I was a normal random user I wouldn't know who EDGE was
-    // "with Git" sorry what
+    // "with Git" sorry what - odyssey
   setInterval(async () => {
     let index = Math.floor(Math.random() * statusList.length + 1) - 1;
     await client.user.setActivity(statusList[index].msg, {
@@ -30,13 +30,13 @@ module.exports = async client => {
   let servers = client.guilds.cache.size;
   let channels = client.channels.cache.size;
 
-  //Logs the Status
+  // logs the status
   client.logger.log(`RAM usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, "ready");
   client.logger.log(`Users: ${users}`, "ready");
   client.logger.log(`Servers: ${servers}`, "ready");
   client.logger.log(`Channels: ${channels}`, "ready");
   client.logger.log(`Running on Node ${process.version.replace(/ /g, "")}`, "ready");
-  client.logger.log(`and using Discord.js v${require("discord.js").version.replace(/ /g, "")}`, "ready");
+  client.logger.log(`...and using Discord.js v${require("discord.js").version.replace(/ /g, "")}`, "ready");
 
   client.logger.log("Cytrus-RE V" + require("../package").version + " | https://github.com/Cytrus-RE/cytrus-re");
   client.startuptime = new Date().getTime() - client.starttime;
