42c32b0ada808a9c431ecdbead07cd0c865eb9b6
hanamijung <hanamispkz@gmail.com>
2020-08-27 23:41:47 +0700

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,21 +4,8 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   try {
     let msgPing = Math.floor(Date.now() - message.createdTimestamp);
 
-    message.channel.send(`[🛰️] Kato Ping: ${msgPing}ms\n[📨] Message Ping: ${Math.round(client.ws.ping)}ms`);
+    message.channel.send(`[🛰️] **Kato Ping**: ${msgPing}ms\n[📨] **Message Ping**: ${Math.round(client.ws.ping)}ms`);
     
-    let embed = new Discord.MessageEmbed()
-    .setTitle(user.user.username)
-    .setDescription(`**ID**: ${user.id}
-Name: ${user.user.username}
-Icon URL: ${user.user.avatarURL}
-Account Created At: ${user.user.createdAt}
-Game: ${user.user.presence.game || "none"}
-Status: ${user.user.presence.status.toUpperCase()}
-Full Name: ${user.user.tag}`)
-    .setThumbnail(user.user.avatarURL)
-    .setColor("#eeeeee");
-    
-    message.channel.send(embed);
   } catch (err) {
     message.channel.send(client.errors.genericError + err).catch();
   }
