2f22885972ecfec2247333b4875f246f86acebf6
CommandBlock6417 <pgkoutsoumanis@gmail.com>
2020-08-27 18:55:08 +0300

    Added new command: hexcolorview
    Allows the user to visualise any hex color they specify

---
+++ src/commands/hexcolorview.js
@@ -0,0 +1,25 @@
+exports.run = async (client, message, args, level) => {
+  try {
+    if (!args[0]) return message.channel.send("You have to input the HEX code!");
+      
+    let hexcolor = "\"" + args[0] "\""
+
+    message.channel.send({ embed: { color: `${hexcolor}`, title: `${hexcolor}` }});
+  } catch (err) {
+    message.channel.send(client.errors.genericError + err).catch();
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  aliases: [hexview],
+  guildOnly: false,
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "hexcolorview",
+  category: "General",
+  description: "Sends an embed with the color of the hex value you specified",
+  usage: "hexcolorview <value>"
+};
