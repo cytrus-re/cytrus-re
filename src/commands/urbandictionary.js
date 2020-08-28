2f1aef868feb470991f05f0849c829b4f67dde59
Ed L <beartechtalks@gmail.com>
2020-08-28 17:01:33 +0100

    Update urbandictionary.js

--- src/commands/urbandictionary.js
+++ src/commands/urbandictionary.js
@@ -3,24 +3,24 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-    if (!message.channel.nsfw) return message.reply("You need to be in a channel marked as NSFW to use this command!");
+    if (!message.channel.nsfw) return message.channel.send("You need to be in an NSFW channel use this command!");
     
     ud.term(args.join(" ")).then(async (result) => {
-      if (!args[0]) return message.channel.send("You need to provide a search term!");
+      if (!args[0]) return message.channel.send("You need to give me a word!");
       let output = "";
       let entries = result.entries;
       let i = 1;
       
-      if (entries == []) return message.channel.send("I didn't find any results for " + args.join(" ") + ".");
+      if (entries == []) return message.channel.send(`I couldn't find any results for ${args.join(" ")}!`);
       
       Object.keys(entries).forEach(async (pageID) => {
         output += "\n" + i + ". " + entries[pageID].word;
         i++;
       });
       
-      let page = await client.awaitReply(message, "Please choose the page you want:\n${output}");
+      let page = await client.awaitReply(message, `Please choose the page you want:\n${output}`);
       
-      if (isNaN(page)) return message.reply(page + " is not a number!");
+      if (isNaN(page)) return message.channel.send(`${page} is not a number!`);
       
       let embed = new Discord.MessageEmbed()
       .setTitle(entries[page - 1].word)
@@ -44,7 +44,7 @@ exports.conf = {
 
 exports.help = {
   name: "urbandictionary",
-  category: "General",
-  description: "Searches the Urban Dictionary for you.",
-  usage: "urbandictionary <term>"
+  category: "Search",
+  description: "Searches the Urban Dictionary for your query.",
+  usage: "urbandictionary <word/phrase>"
 };
