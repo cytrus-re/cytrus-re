c4989993fa04aba2132cc26485b78578fc0364e5
Ed L <beartechtalks@gmail.com>
2020-08-28 16:51:39 +0100

    Update messageDelete.js

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports = (client, message) => {
-  if (message.author.bot) return;
+  if (message.author.bot || !message.guild) return;
   
   let settings = client.getSettings(message.guild.id);
   if (settings.logMessageUpdates == "true") {
