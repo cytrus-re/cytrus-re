cb7174b90de31a89a683da610294fb93b66650a9
CommandBlock6417 <pgkoutsoumanis@gmail.com>
2020-08-28 12:29:57 +0300

    changed name of temp conversion command because it was unclear

--- src/commands/converttemp.js
+++ src/commands/converttemp.js
@@ -35,7 +35,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 
 exports.conf = {
   enabled: true,
-  aliases: ["celfar", ],
+  aliases: ["celfar", "farcel"],
   guildOnly: true,
   permLevel: "User"
 };
