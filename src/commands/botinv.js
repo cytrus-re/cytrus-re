7a199a6d08384e1023a8ee7663fcf20de24fcb14
hanamijung <hanamispkz@gmail.com>
2020-08-28 12:33:48 +0700

    Update botinv.js

--- src/commands/botinv.js
+++ src/commands/botinv.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
     let invEmbed = new Discord.MessageEmbed()
-    .setTitle("Invite Cytrus-RE to your server!")
-    .setDescription("[**Click here!**](https://discordapp.com/api/oauth2/authorize?client_id=596304769333592078&permissions=2113404023&scope=bot)")
+    .setTitle("Invite Kato-Bot to your server!")
+    .setDescription("[**Click here!**](https://discordapp.com/api/oauth2/authorize?client_id=739817077573025892&permissions=2113404023&scope=bot)")
     .setColor("#eeeeee")
     message.channel.send(invEmbed);
   } catch (err) {
