b0fe93c21d9cac3860cf77796831de3efd997275
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-08-28 17:40:07 +0200

    Ok maybe this will work?

--- src/commands/passwordstrength.js
+++ src/commands/passwordstrength.js
@@ -2,7 +2,8 @@ const zxcvbn = require("zxcvbn");
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-    if (message.guild) return message.channel.send("For security reasons, this command is not available in servers. Please run this command in DM's.");
+    if (message.guild): 
+      message.channel.send("For security reasons, this command is not available in servers. Please run this command in DM's.");
       message.delete(); //delete the command usage.
     if (!args[0]) return message.reply("You need to give the password!");
     
