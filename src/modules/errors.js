942812ea060f54a3dc028d359b08ab103c859241
CommandBlock6417 <pgkoutsoumanis@gmail.com>
2020-08-29 13:53:15 +0300

    I'm a dev too

--- src/modules/errors.js
+++ src/modules/errors.js
@@ -1,5 +1,5 @@
 const errors = {
-  "genericError": "There was an error!\nPlease send the following to a Cytrus-RE dev (Odyssey346#9848 or Rexowogamer#1183) and tell them command you used (including arguments).\n---------------------------------------------------------------------------------------------------------------------\n",
+  "genericError": "There was an error!\nPlease send the following to a Cytrus-RE dev (Odyssey346#9848, Rexowogamer#1183 or Commandblock6417#9366) and tell them command you used (including arguments).\n---------------------------------------------------------------------------------------------------------------------\n",
   "wiiError": "An error has occurred.\nPress the Eject button, remove the Game Disc, and turn off the power to the console.\nPlease read the Wii Operations Manual for further instructions.",
   "userNotInGuild": "This user isn't in this guild!",
   "cannotBanSelf":  "Sorry, you can't ban yourself. (It would be funny, if it wasn't so sad.)",
