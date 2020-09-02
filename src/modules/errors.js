49c050eb2cbea6868b6c2ac0695d09f51df24a2e
Ed L <beartechtalks@gmail.com>
2020-09-02 09:13:55 +0100

    Update errors.js

--- src/modules/errors.js
+++ src/modules/errors.js
@@ -3,8 +3,9 @@ const errors = {
   "wiiError": "An error has occurred.\nPress the Eject button, remove the Game Disc, and turn off the power to the console.\nPlease read the Wii Operations Manual for further instructions.",
   "userNotInGuild": "This user isn't in this guild!",
   "cannotBanSelf":  "Sorry, you can't ban yourself. (It would be funny, if it wasn't so sad.)",
-  "cannotKickSelf":  "Sorry, you can't kick yourself. (If you don't want to be there, just leave.)",
+  "cannotKickSelf":  "Sorry, you can't kick yourself. (If you don't want to be here, just leave.)",
   "noResults": "I couldn't find any results for your query!",
+  "nsfwOnly": "You need to be in an NSFW channel to use this command!",
   "noArticleDescription": "This article doesn't have a description or something went wrong.",
   "cannotPingEveryone": "Sorry, you cannot mention everyone or here with say, for obvious reasons. (If it isn't obvious, we don't want chaos)",
   "mathError": "Sorry, you did something wrong with your maths.\n---------------------------------------------------------------------------------------------------------------------\n"
