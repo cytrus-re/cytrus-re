2cda34b785696f1c2824f11076f03e2b81ccc7a5
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-09-04 18:10:29 +0200

    maybe we can log the error to the logging channel?

--- src/events/error.js
+++ src/events/error.js
@@ -1,4 +1,5 @@
 module.exports = async (client, error) => {
   if (JSON.stringify(error).toLowerCase().includes("discordapierror")) return; 
   client.logger.log(error.message, "error"); // Log error
+  client.channels.cache.get("751473673474539560").send(error.message);
 };
