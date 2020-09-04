861fbe050db920529b668c29a41a423c4927738f
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-09-04 18:18:10 +0200

    i'm banging my head to the wall now

--- src/events/error.js
+++ src/events/error.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 module.exports = async (client, error) => {
   if (JSON.stringify(error).toLowerCase().includes("discordapierror")) return; 
   client.logger.log(error.message, "error"); // Log error
