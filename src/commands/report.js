0cfc09756e17f822213748bd3ad871116a6f3ce1
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-10-01 08:46:13 +0200

    report: indentation
    disgusting

--- src/commands/report.js
+++ src/commands/report.js
@@ -2,35 +2,36 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args, level) => { 
   try {
   //command code
-  
-let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));    
-if (!rUser) return message.channel.send("I couldn't find the specified user!");
-console.log("user exists!"); 
-let rreason = args.join(" ").slice(22);
+
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));    
+    if (!rUser) return message.channel.send("I couldn't find the specified user!");
+    console.log("user exists!"); 
+    let rreason = args.join(" ").slice(22);
 
 
-let reportEmbed = new Discord.MessageEmbed()
- .setAuthor("Cytrus-RE User Report")
- .setColor("#eeeeee")
- .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
- .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
- .addField("Reported in", message.channel)
- .addField("Reported at", message.createdAt)
- .addField("Reason for report", rreason);
+    let reportEmbed = new Discord.MessageEmbed()
+     .setAuthor("Cytrus-RE User Report")
+     .setColor("#eeeeee")
+     .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+     .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+     .addField("Reported in", message.channel)
+     .addField("Reported at", message.createdAt)
+     .addField("Reason for report", rreason);
 
- let reportschannel = client.channels.get("691142562253242409");
- if(!reportschannel) return message.channel.send("I couldn't find the reports channel!");
- console.log("channel exists!"); 
+     let reportschannel = client.channels.get("691142562253242409");
+     if(!reportschannel) return message.channel.send("I couldn't find the reports channel!");
+     console.log("channel exists!"); 
 
- message.delete();
- console.log("message deleted!"); 
- reportschannel.send(reportEmbed);
- console.log("report message sent!");
- message.channel.send("Report successfully sent!");
+     message.delete();
+     console.log("message deleted!"); 
+     reportschannel.send(reportEmbed);
+     console.log("report message sent!");
+     message.channel.send("Report successfully sent!");
 
 //error log
   
   } catch (err) {
+    // oops my pc crashed and i lost all my data, and i had an antivirus!
     message.channel.send(client.errors.genericError + err).catch();
   }
 };
