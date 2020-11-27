f28b4ca2e26965ab42ca90d2d9d9766883325ee8
Ed L <beartechtalks@gmail.com>
2020-11-27 18:10:51 +0000

    fix (info): aaaaAAA

--- src/commands/info.js
+++ src/commands/info.js
@@ -12,7 +12,7 @@ exports.run = async (client, message, args, level) => {
     .setDescription(`Github: [Repo](${client.config.github})
 Website: [Check it out](https://cytrus-re.github.io)
 Support Server: [Join](${client.config.supportServer})
-Issues: [Right here](${client.conf.github}/issues)
+Issues: [Right here](${client.config.github}/issues)
 Version: V1.3
 Currently running on: ${cleanOS}`);
     message.channel.send(embed);
