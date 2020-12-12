713d329e03e602d2f624f10d25c9984fbbb68449
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-12-12 15:39:32 +0100

    Update cnf.js

--- src/cnf.js
+++ src/cnf.js
@@ -4,7 +4,7 @@ const config = {
   "owners": ["395990735934980097"], // rexo has full power ahahahaha
   "managers": ["429353559566319626"], // commandblock
   "admins": ["390663607849058307"], // derser
-  "devs": ["315843700490240002"], //Odyssey346 (prob controversial lol)
+  "devs": ["315843700490240002", "486899102760697856"], //Odyssey346 & his alt
   "mods": [], 
   "support": [],
   "helpers": [],
