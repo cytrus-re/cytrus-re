03527a8bc3e978cf8f292f8c2a39a2ccd7d6470d
Oxygemo <alej0hio2007@gmail.com>
2020-12-26 17:56:48 +0100

    help niw
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/commands/debugsay.js
+++ src/commands/debugsay.js
@@ -2,20 +2,16 @@ exports.run = async (client, message, args) => {
     try {
       message.delete().catch();
       const mg = args.join(" ");
-      if (message.mentions.everyone) {
-        message.delete().catch();
-        message.channel.send("Function read");
-        message.channel.send(client.errors.cannotPingEveryone); 
-        return; //send error and return early if the message includes mentions.
-      }
-      if (mg = '@everyone') {
+      if (mg = '@everyone', '@here') {
         message.delete().catch();
         message.channel.send("Function2 read");
         message.channel.send("Nice try!");
         return; 
       }
-      message.channel.send("Function potentially skipped");
-      message.channel.send(mg);
+      else {
+        message.channel.send("Function potentially skipped");
+        message.channel.send(mg);
+      }
     } catch (err) {
       message.channel.send(client.errors.genericError + err).catch();
     }
