3a80b9aedaffe0bc1bc927cc37ac5b49d94f82cd
Oxygemo <alej0hio2007@gmail.com>
2021-01-31 15:44:55 +0100

    fix(logging command): grammar

--- src/commands/log.js
+++ src/commands/log.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args, level) => {
   try {
     if (client.liusers.has(message.author.id)) {
       client.logger.log(message.author.id + " | " + args.join(" "), "user");
-      message.channel.send("Message sent to the Cytrus Logs!");
+      message.channel.send("Message sent to the Cytrus-RE Logs!");
     } else message.reply("You are not logged in! (Use profile login to login)");
   } catch (err) {
     message.channel.send(client.errors.genericError + err).catch();
@@ -19,6 +19,6 @@ exports.conf = {
 exports.help = {
   name: "log",
   category: "General",
-  description: "Sends a message to the CytrusLog",
+  description: "Sends a message to the Cytrus-RE Log",
   usage: "log"
 };
